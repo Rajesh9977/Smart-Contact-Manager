005572cf423d34a40f17ace359b0ee2ba39bbb2e
Rajesh Rao <nontechcoding@gmail.com>
2025-09-15 09:10:00 +0530

    (setup)-Basic setup

--- src/main/java/com/scm/scm/ScmApplication.java
+++ src/main/java/com/scm/scm/ScmApplication.java
@@ -8,8 +8,6 @@ public class ScmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScmApplication.class, args);
-		System.out.print("hello");
-		System.out.print("hello");
 	}
 
 }
